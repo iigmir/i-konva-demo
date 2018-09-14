7abb19aa95b2195288e832aeb9ddcda5526ce8e1
iigmir <roc120j@gmail.com>
2018-09-14 09:23:14 +0800

    bobob

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <navbar />
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import navbar from "@/components/navbar.vue";
+
+export default {
+    components: { navbar }
+}
+</script>
+
 
 <style lang="scss">
 #app {
@@ -15,15 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Vue Konva app"/>
+    </div>
 </template>
 
 <script>
