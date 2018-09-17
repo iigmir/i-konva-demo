923f6dd0790a433bdeb4d92acea70b9a2630d37a
iigmir <roc120j@gmail.com>
2018-09-17 11:29:50 +0800

    issues

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <p>未解決問題</p>
         <ol>
             <li> 重新繪製不成功：會同時出現兩個圖 </li>
+            <li> 無法針對形狀使用本地圖像如：<code>@/assets/image.jpg</code> </li>
         </ol>
     </div>
 </template>
