0348cb0e55b315c3ac589de2ffd7b784232cd1eb
iigmir <roc120j@gmail.com>
2018-09-18 11:40:15 +0800

    angered

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,7 +3,7 @@
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link> |
         <router-link to="/proto1">Prototype 1</router-link> |
-        <router-link to="/proto3">Prototype 3</router-link> |
+        <!-- <router-link to="/proto3">Prototype 3</router-link> | -->
         <router-link to="/proto4">Prototype 4</router-link> |
         <router-link to="/proto5">Prototype 5</router-link> |
         <router-link to="/ghouse">Greatest house</router-link> |
