8702826621df32646eb9269f1b759a36b32e5470
iigmir <roc120j@gmail.com>
2018-09-18 10:45:45 +0800

    sdf

--- src/views/Animate.vue
+++ src/views/Animate.vue
@@ -13,6 +13,31 @@
                 }" />
             </v-layer>
         </v-stage>
+        <v-stage ref="stage" :config="{ width: 300, height: 200 }">
+            <v-layer ref="layer">
+                <v-regular-polygon
+                    @mousemove="handleMouseMove"
+                    @mouseout="handleMouseOut"
+                    :config="{
+                        x: 80,
+                        y: 120,
+                        sides: 3,
+                        radius: 80,
+                        fill: '#00D2FF',
+                        stroke: 'black',
+                        strokeWidth: 4
+                    }"
+                />
+                <v-text ref="text" :config="{
+                    x: 10,
+                    y: 10,
+                    fontFamily: 'Calibri',
+                    fontSize: 24,
+                    text: '',
+                    fill: 'black'
+                }" />
+            </v-layer>
+        </v-stage>
     </div>
 </template>
 
@@ -31,6 +56,21 @@ export default {
             vm.$refs.hexagon.getStage().setX(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + centerX);
         }, vm.$refs.layer.getStage());
         anim.start();
+    },
+    methods: {
+        writeMessage(message) {
+            this.$refs.text.getStage().setText(message);
+            this.$refs.layer.getStage().draw();
+        },
+        handleMouseOut(vueComponent, event) {
+            this.writeMessage('Mouseout triangle');
+        },
+        handleMouseMove(vueComponent, event) {
+            const mousePos = this.$refs.stage.getStage().getPointerPosition();
+            const x = mousePos.x - 190;
+            const y = mousePos.y - 40;
+            this.writeMessage('x: ' + x + ', y: ' + y);
+        }
     }
 }
 </script>
