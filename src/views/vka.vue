50fb309ae2c2e6db4f1fba424fe14ca789378a67
iigmir <roc120j@gmail.com>
2018-09-20 10:17:47 +0800

    sdf

--- src/views/vka.vue
+++ src/views/vka.vue
@@ -5,14 +5,6 @@
             <li> 前面說過 Vue + Konva == vue-konva  </li>
             <li>
                 你可以使用 HTML 的語法，來撰寫 Konva 的元件：
-                <pre>
-&lt;v-rect v-bind:config="{
-    //
-}" /&gt;
-                </pre>
-            </li>
-            <li>
-                上例的成果如下：
                 <v-stage v-bind:config="{ width:100, height:100 }">
                     <v-layer>
                         <v-rect v-bind:config="{
@@ -38,7 +30,32 @@
                         }" />
                     </v-layer>
                 </v-stage>
-            </li>
+                <pre>
+&lt;v-stage v-bind:config="{ width:100, height:100 }"&gt;
+    &lt;v-layer&gt;
+        &lt;v-rect v-bind:config="{
+            width: 100,
+            height: 100,
+            fill: 'black',
+        }" /&gt;
+        &lt;v-circle v-bind:config="{
+            x:50,
+            y:50,
+            radius: 40,
+            fill: 'navy',
+            stroke: 'white',
+            strokeWidth: 4
+        }" /&gt;
+        &lt;v-star v-bind:config="{
+            x: 50,
+            y: 50,
+            numPoints: 5,
+            innerRadius: 10,
+            outerRadius: 30,
+            fill: 'red',
+        }" /&gt;
+    &lt;/v-layer&gt;
+&lt;/v-stage&gt; </pre> </li>
             <li> 形狀支援還不夠完整，但也足夠撰寫一些基本形狀：
                 <ul>
                     <li>v-rect: 四方形</li>
