8837c4b18b705f5243ed517fe1368846917ab563
iigmir <roc120j@gmail.com>
2018-09-21 13:59:23 +0800

    pto1 wip

--- src/views/prototype/Proto1.vue
+++ src/views/prototype/Proto1.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <p>PPPP</p>
-        <!-- <v-stage ref="currentstage" v-bind:config="$store.state.canvas_measure">
-            <v-layer ref="imgs">
-                <v-image v-bind:config="configImg"/>
-                <v-image v-bind:config="logo_ing"/>
+        <v-stage ref="currentstage" v-bind:config="$store.state.canvas_measure">
+            <v-layer>
+                <v-rect
+                    v-for="(item, index) in rects"
+                    v-bind:key="index"
+                    v-bind:config="item"
+                    v-on="rect_events" />
             </v-layer>
-        </v-stage> -->
+        </v-stage>
     </div>
 </template>
 
@@ -19,6 +22,32 @@ export default {
         };
     },
     computed: {
+        rects()
+        {
+            let foo = [];
+            let colours = ["red", "orange", "yellow", "green", "blue", "purple"];
+            for(var i = 0; i < 6; i++)
+            {
+                foo.push({
+                    x: i * 30 + 50,
+                    y: i * 18 + 40,
+                    fill: colours[i],
+                    stroke: "black",
+                    strokeWidth: 4,
+                    draggable: true,
+                    width: 100,
+                    height: 50
+                });
+            }
+            return foo;
+        },
+        rect_events()
+        {
+            return {
+                mouseover: () => document.body.style.cursor = "pointer",
+                mouseout: () => document.body.style.cursor = "default",
+            };
+        }
     },
     created()
     {
